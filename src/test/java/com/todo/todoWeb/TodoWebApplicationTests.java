166fe72db06d96df5496794bf7ad03234a94580f
wjdtj9656 <wjdtj9656@naver.com>
2022-05-05 23:12:50 +0900

    start project

---
+++ src/test/java/com/todo/todoWeb/TodoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
